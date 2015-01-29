6cc778829db13af22411a25bc30c4c189f49a136
Ronan Brett <purplejunkie.com@gmail.com>
2015-01-29 14:20:52 +1100

    first commit

--- accc.sql
+++ accc.sql
@@ -1,11 +1,7 @@
 PRAGMA foreign_keys=OFF;
 BEGIN TRANSACTION;
-CREATE TABLE cpdaccreditors ( cpdaccreditor text );
 INSERT INTO "cpdaccreditors" VALUES('ARGGHH');
-CREATE TABLE cpdcategories ( cpdcategory text );
 INSERT INTO "cpdcategories" VALUES('5');
-CREATE TABLE mealtypes ( mealtype text );
 INSERT INTO "mealtypes" VALUES('Tasty Food Time');
-CREATE TABLE meetingtypes ( meetingtype text );
 INSERT INTO "meetingtypes" VALUES('Super Happy Disco Ball');
 COMMIT;
